5089240840bbbc98a23f8bb904ee2c91139bb7f7
anmolgoyal1212 <anmolgoyal_ee21b6_59@dtu.ac.in>
2024-10-12 05:06:20 +0530

    add userModel and videoModel

---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import { Schema, Model } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //Cloudinary
+      required: true,
+    },
+    thumbnail: {
+      type: String, //Cloudinary
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    duration: {
+      type: Number, //Cloudinary
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = Model("Video", videoSchema);
