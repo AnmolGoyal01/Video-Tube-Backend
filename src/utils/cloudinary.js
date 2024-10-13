f8e437e17e42b1f80f93473d524192c1db927a17
anmolgoyal1212 <anmolgoyal_ee21b6_59@dtu.ac.in>
2024-10-13 23:53:17 +0530

    unlink file after uploading on cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,6 +17,7 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     console.log("File is uploaded on Cloudinary");
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath);
